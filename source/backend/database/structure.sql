8deacc0f2e7f0928455dfbc2cff62decc5a69ecf
Mauricio Lima <github@mauriciolima.eti.br>
2020-01-02 02:27:47 -0300

    Initial database structure

---
+++ source/backend/database/structure.sql
@@ -0,0 +1,30 @@
+/*
+SQLyog Community v13.0.0 (64 bit)
+MySQL - 5.6.41-84.1 : Database - mauri879_financial_nfp
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `input` */
+
+DROP TABLE IF EXISTS `input`;
+
+CREATE TABLE `input` (
+  `input_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `content` mediumblob,
+  `processed` tinyint(1) DEFAULT '0' COMMENT 'will signal if the registration has already been processed',
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`input_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
