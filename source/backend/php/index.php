5ca19cf4c9b4ca9dff26161186dd2d3b0c74cc6c
Mauricio Lima <github@mauriciolima.eti.br>
2020-01-02 02:50:17 -0300

    remote service storing requests and listing all without processing

--- source/backend/php/index.php
+++ source/backend/php/index.php
@@ -3,41 +3,144 @@
 
   $logger = new Logger('log.txt');
   
-  header('Content-type: text/plain');
+  
+  function GetDatabaseConnection()
+   {
+    $database = new PDO('mysql:host=localhost; dbname=mauri879_financial_nfp', 'mauri879_phpscpt', 'phpscpt');
+    $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    $database->query('SET character_set_connection=utf8');
+    $database->query('SET character_set_client=utf8');
+    $database->query('SET character_set_results=utf8');
+    
+    return $database;
+   }
+
+
+  function listCoupons()
+  {
+    header('Content-type: text/plain');
+  
+    $sql = 'SELECT content FROM input';
+    try 
+    {
+      $database = GetDatabaseConnection();
+      $statment = $database->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
+      $statment->execute();
+      
+      while ($row = $statment->fetch(PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT)) {
+        $data = json_decode($row['content'], false);
+        if (json_last_error() != JSON_ERROR_NONE)
+        {
+          print('JSON Decode Error : ' . json_last_error());   
+          throw new Exception('JSON Decode Error');
+        }
+        
+        //print_r($data);
+        $display  = '';
+        $display .= ($data->customer->documents->cpf_cnpj != '') ? $data->customer->documents->cpf_cnpj : 'missing    ';
+        //$display .= $data->documents->cnpj_cpf;
+        $display .= '  ';
+        $display .= substr($data->datetime,0,10) . '     ';
+        $display .= substr('       ' . $data->totals->total, -8) . '    '; 
+        $display .= $data->store->name;
+        print($display . PHP_EOL);
+       
+        //print($row['content']);
+        //print($data['totals']['total'] . PHP_EOL);
+      }
+    }
+    catch (PDOException $e) 
+    {
+      print($e->getMessage());
+    }
+    catch (Exception $e)
+    {
+      print('Generic exception');
+    }
+    finally
+    {
+      $statment = null;
+      print(PHP_EOL);
+    }
+  }
+
+ 
+  function get($uri)
+  {
+     switch ($uri)
+     {
+       case '/nfp/list':
+         listCoupons();
+         break;
+         
+       default:
+         header('Access-Control-Allow-Origin: *');     
+         header('Content-type: application/json');
+         $result = array(
+                          'error'   => 'Unmapped URI',
+                          'referer' =>  $_SERVER['HTTP_REFERER'],
+                          'uri'     =>  $_SERVER['REQUEST_URI' ]
+                        );
+         print(json_encode($result, JSON_PRETTY_PRINT));
+         
+     }
+  }
+
+  
+  function post()
+  {
+    global $logger;
+    
+    $body = file_get_contents('php://input');
+  
+    $sql  = '';
+    $sql .= 'insert into `input`'    . PHP_EOL;
+    $sql .= '   (content)' . PHP_EOL;
+    $sql .= 'values'                          . PHP_EOL;
+    $sql .= '   (';
+    $sql .= '     \'' . $body . '\'';
+    $sql .= '   )';
+    
+    $dump = '';
+    try
+     { 
+      $database = GetDatabaseConnection(); 
+      $statment = $database->prepare($sql);
+      $statment->execute();
+      
+      $logger->put('ok');
+      $result = array( 'message' => 'input registered' );
+     }
+    catch(PDOException $err)
+     {
+      $dump .= $err->getMessage()     . PHP_EOL;
+      $dump .= $database->errorInfo() . PHP_EOL;
+      $logger->put($dump);   
+      $result = array( 'message' => $err->getMessage());
+     }
+     
+    header('Content-type: application/json');
+    print(json_encode($result, JSON_PRETTY_PRINT));
+  }
+
+
   switch ($_SERVER['REQUEST_METHOD'])
   {
     case 'OPTIONS':
         header('Access-Control-Allow-Origin: *');
   	header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-  	header('Access-Control-Allow-Headers: Content-Type');
-  	
+  	header('Access-Control-Allow-Headers: Content-Type');	
   	$logger->put('OPTIONS');
   	die();
   	
     case 'POST':
        header('Access-Control-Allow-Origin:  *');
-       $body = file_get_contents('php://input');
-       print($_SERVER['CONTENT_TYPE'] . PHP_EOL);
-       print(PHP_EOL);
-       print($body);  
-       $logger->put('POST' . PHP_EOL . $_SERVER['CONTENT_TYPE'] . PHP_EOL . $body);
-       die();
+       post();
+       break;
        
     case 'GET':
-       header('Access-Control-Allow-Origin: *');     
-       header('Content-type: application/json');
-       $result = array(
-                        'error' => 'Only POST method accepted for this service : ' . $_SERVER['REQUEST_METHOD'],
-                        'referer' => $_SERVER['HTTP_REFERER'],
-                        'uri'     => $_SERVER['REQUEST_URI' ]
-                      );
-       print(json_encode($result, JSON_PRETTY_PRINT));
-       die();
-          	
-       print ('Only POST method accepted for this service : ' . $_SERVER['REQUEST_METHOD']);
-       print (PHP_EOL . 'Referer : ' . $_SERVER['HTTP_REFERER']);
-       print (PHP_EOL . 'URI     : ' . $_SERVER['REQUEST_URI' ]);
-       $logger->put('GET' . PHP_EOL . 'Host : ' . $_SERVER['HTTP_REFERER']);
+       get($_SERVER['REQUEST_URI' ]);
        die();
        
     default:
@@ -45,6 +148,6 @@
        $logger->put($_SERVER['REQUEST_METHOD'] . PHP_EOL . 'Method not processed' );
        die();
        
-  } 
-  	
+  }
+
 ?>
