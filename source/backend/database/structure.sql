ddde62d297083390f12f29882b13dd66ba147435
Mauricio Lima <github@mauriciolima.eti.br>
2020-01-03 22:38:38 -0300

    Table 'customers' defined

--- source/backend/database/structure.sql
+++ source/backend/database/structure.sql
@@ -12,6 +12,20 @@ MySQL - 5.6.41-84.1 : Database - mauri879_financial_nfp
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `customer` */
+
+DROP TABLE IF EXISTS `customer`;
+
+CREATE TABLE `customer` (
+  `customer_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `cpf_cnpj` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `name` varchar(70) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `address` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `approved` tinyint(1) DEFAULT '0',
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`customer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 /*Table structure for table `input` */
 
 DROP TABLE IF EXISTS `input`;
@@ -37,7 +51,7 @@ CREATE TABLE `stores` (
   `im` varchar(15) COLLATE utf8_unicode_ci DEFAULT NULL,
   `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`store_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
